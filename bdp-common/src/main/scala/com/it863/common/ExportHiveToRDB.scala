993464881969570f38d6fc9987617d4f1a3a4476
zenithwu <wzhwei@aliyun.com>
2018-09-17 13:45:23 +0800

    l

--- bdp-common/src/main/scala/com/it863/common/ExportHiveToRDB.scala
+++ bdp-common/src/main/scala/com/it863/common/ExportHiveToRDB.scala
@@ -2,52 +2,55 @@ package com.it863.common
 
 import java.util.Properties
 
-import com.it863.common.util.JdbcUtil.urlFormat
+import com.it863.common.util.JdbcUtil._
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.{SaveMode, SparkSession}
 
-object ExportHiveToMysql {
+object ExportHiveToRDB {
 
   def main(args: Array[String]): Unit = {
-
     val jobid = args(0)
     val timehour = args(1)
 
-    val hdfsPath = "hdfs:///bdp/jobconfig/jobid_${jobid}.json"
+    val hdfsPath = "hdfs:///bdp/jobconfig/jobid_" + jobid + ".json"
 
     val conf = new SparkConf().setAppName("ExportHiveToMysql")
     val spark = SparkSession.builder().config(conf).enableHiveSupport().getOrCreate()
-
-    val row = spark.read.json("hdfs:///bdp/jobconfig/jobid_45.json").first()
+    val row = spark.read.json(hdfsPath).first()
     val jobConfig = row.getValuesMap(row.schema.fieldNames)
-
     // 获取参数
     val hiveQL = jobConfig.getOrElse("input_input_content", "")
     val saveMode = jobConfig.getOrElse("output_write_model", "overwrite")
     val jdbcUser = jobConfig.getOrElse("username", "")
     val jdbcPass = jobConfig.getOrElse("password", "")
     val jdbcHost = jobConfig.getOrElse("host", "")
-    val jdbcPort = jobConfig.getOrElse("port", 3306).toInt
+    val jdbcPort = jobConfig.getOrElse("port", "3306")
     val dbType = jobConfig.getOrElse("type", "").toLowerCase
-    val driver = jobConfig.getOrElse("driver", null)
+    val driver = jobConfig.getOrElse("driver", "")
     val dbName = jobConfig.getOrElse("dbname", "")
-
-    val preSql = jobConfig.getOrElse("output_pre_statment", null)
-    val targetTable = jobConfig.getOrElse("output_table_name", null)
+    val preSql = jobConfig.getOrElse("output_pre_statment", "")
+    val targetTable = jobConfig.getOrElse("output_table_name", "")
 
     val url = urlFormat(dbType, jdbcHost, jdbcPort, dbName)
-
 
     // 使用别名方式
     import spark.{sql => hsql}
     val hiveDF = hsql(s"${hiveQL}")
+
+
+    // 执行前置sql
+    if (preSql != "") {
+      runSQL(driver, preSql, url, jdbcUser, jdbcPass)
+    }
+
 
     //设置mysql连接
     val prop = new Properties()
     prop.setProperty("user", jdbcUser)
     prop.setProperty("password", jdbcPass)
 
-    hiveDF.write.mode(SaveMode.valueOf(saveMode)).jdbc(url, targetTable, prop)
+    val sm = if (saveMode == "append") SaveMode.Append else SaveMode.Overwrite
+    hiveDF.write.mode(sm).jdbc(url, targetTable, prop)
 
     spark.stop()
   }
--- bdp-common/src/main/scala/com/it863/common/util/JdbcUtil.scala
+++ bdp-common/src/main/scala/com/it863/common/util/JdbcUtil.scala
@@ -1,11 +1,13 @@
 package com.it863.common.util
 
+import java.sql.{Connection, DriverManager, Statement}
+
 object JdbcUtil {
   def runPreSQL(dbType: String, preSQL: String): Int = {
     return 0
   }
 
-  def urlFormat(dbType: String, jdbcHost: String, jdbcPort: Int, dbName: String): String = {
+  def urlFormat(dbType: String, jdbcHost: String, jdbcPort: String, dbName: String): String = {
     val url = dbType match {
       case "mysql" => s"jdbc:mysql://${jdbcHost}:${jdbcPort}/${dbName}"
       case "oracle" => s"jdbc:oracle:thin:@//${jdbcHost}:${jdbcPort}/${dbName}"
@@ -15,9 +17,38 @@ object JdbcUtil {
     return url
   }
 
-  def runSQL(driver: String, sql: String) = {
+  def runSQL(driver: String, sql: String, url: String, user: String, password: String): Unit = {
+    if (driver == "") {
+      throw new RuntimeException("driver is null")
+    }
+    val sqlList = sql.split(";")
 
+    var connection: Connection = null
+    var statement: Statement = null
+
+    try {
+      Class.forName(driver)
+      connection = DriverManager.getConnection(url, user, password)
+      statement = connection.createStatement()
+      sqlList.foreach(sql => statement.execute(sql.trim))
+
+    } catch {
+      case e => e.printStackTrace()
+    } finally {
+      statement.closeOnCompletion()
+      connection.close()
+    }
   }
 
+
+  def main(args: Array[String]): Unit = {
+    val driver = "com.mysql.jdbc.Driver"
+    val sql = "DELETE FROM sysmenu_callback WHERE id = 105;DELETE FROM sysmenu_callback WHERE id = 106;;;;"
+    val url = "jdbc:mysql://192.168.0.203:3306/bdp"
+    val user = "bdp"
+    val password = "bdp"
+
+    runSQL(driver, sql, url, user, password)
+  }
 
 }
